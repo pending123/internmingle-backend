984f62457db90e96b0f9d5ae4cdd2dadf11fbd2b
Divine Precious-Esue <divinepreciousesue@gmail.com>
2025-07-15 15:29:39 -0700

    Added Cors

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const express = require('express')
 const { clerkClient, requireAuth, getAuth } = require('@clerk/express')
 const neighborhoodRoutes = require("./src/routes/neighborhoodRoutes");
 
+const cors = require("cors");
+
+const corsOption = {
+  origin: "http://localhost:5173",
+};
+
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
+app.use(cors(corsOption));
 
 app.get('/protected', requireAuth(), async (req, res) => {
     const { userId } = getAuth(req)
