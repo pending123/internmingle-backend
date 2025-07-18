4afc283b931e72c1b96feb371ee67ae8f814b158
bfarinango <127805596+bfarinango@users.noreply.github.com>
2025-07-18 13:34:45 -0700

    Brianna/clerk authentication (#7)
    * clerk authentication and user profile creation
    -still need to update with traits and hobbies
    -still need to do webhook testing
    
    * updated profile backend. testing works on clerk's dashboard
    
    ---------
    
    Co-authored-by: Divine <155365475+divineprecious@users.noreply.github.com>

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv/config')
 const express = require('express')
 const { clerkClient, requireAuth, getAuth } = require('@clerk/express')
 const neighborhoodRoutes = require("./src/routes/neighborhoodRoutes");
+const profileRoutes = require('./src/routes/profileRoutes');
+const { webhookHandler } = require('./src/controllers/clerkWebhooks');
+
 
 const cors = require("cors");
 
@@ -12,14 +15,23 @@ const corsOption = {
 const app = express()
 const PORT = 3000
 
+//TEMP__TESTING
+app.post('/api/webhooks', express.raw({ type: 'application/json' }), webhookHandler);
+
+//app.use('/api/webhooks', express.raw({ type: 'application/json' }));
+
 app.use(express.json())
 app.use(cors(corsOption));
+
+//app.post ('/api/webhooks', webhookHandler);
 
 app.get('/protected', requireAuth(), async (req, res) => {
     const { userId } = getAuth(req)
     res.json({userId})
 })
 
+
+app.use('/api/profiles', profileRoutes);
 app.use("/api/neighborhoods", neighborhoodRoutes);
 
 app.get('/', requireAuth(), async (req, res) => {
