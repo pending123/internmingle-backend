502315920b6fd92234aee504316375cd33b05c46
bfarinango <briannafarinango3@gmail.com>
2025-07-18 08:32:42 -0700

    clerk authentication and user profile creation -still need to update with traits and hobbies -still need to do webhook testing

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ require('dotenv/config')
 const express = require('express')
 const { clerkClient, requireAuth, getAuth } = require('@clerk/express')
 const neighborhoodRoutes = require("./src/routes/neighborhoodRoutes");
+const profileRoutes = require('./src/routes/profileRoutes');
+const { webhookHandler } = require('./src/controllers/clerkWebhooks');
+
 
 const app = express()
 const PORT = 3000
 
+app.use('/webhook/clerk', express.raw({ type: 'application/json' }));
+
 app.use(express.json())
+
+app.post ('/webhook/clerk', webhookHandler);
 
 app.get('/protected', requireAuth(), async (req, res) => {
     const { userId } = getAuth(req)
     res.json({userId})
 })
 
+
+app.use('/api/profiles', profileRoutes);
 app.use("/api/neighborhoods", neighborhoodRoutes);
 
 app.get('/', requireAuth(), async (req, res) => {
