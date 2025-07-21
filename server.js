5429f4a0f728a76061e10b5cf1e86d92528b0a25
Mamadou Ndom <ndom15304@gmail.com>
2025-07-21 16:03:53 -0700

    updated event controller and schema.

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-
 const app = express();
-
 const PORT = 3000;
-
 app.use(express.json());
+const cors = require("cors")
 
+const corsOption = {
+    origin: "http://localhost:3000"
+}
+
+const eventRoutes = require("./src/routes/eventRoutes")
+
+
+app.use(cors(corsOption))
+app.use("/", eventRoutes)
 app.get('/', (req, res) => {
     res.send("Hello world!")
 })
