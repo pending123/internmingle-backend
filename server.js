633bf9ca1c4aaf34456e8f8a89afe895f125191c
Mamadou Ndom <ndom15304@gmail.com>
2025-07-22 10:38:14 -0700

    adjusted package.json

--- server.js
+++ server.js
@@ -1,45 +1,50 @@
-
-require('dotenv/config')
-const express = require('express')
-const { clerkClient, requireAuth, getAuth } = require('@clerk/express')
+require("dotenv/config");
+const express = require("express");
+const { clerkClient, requireAuth, getAuth } = require("@clerk/express");
 const neighborhoodRoutes = require("./src/routes/neighborhoodRoutes");
-const profileRoutes = require('./src/routes/profileRoutes');
-const { webhookHandler } = require('./src/controllers/clerkWebhooks');
-
+const profileRoutes = require("./src/routes/profileRoutes");
+const { webhookHandler } = require("./src/controllers/clerkWebhooks");
 
 const cors = require("cors");
 
-const eventRoutes = require("./src/routes/eventRoutes")
-  
+const eventRoutes = require("./src/routes/eventRoutes");
+
 const corsOption = {
   origin: "http://localhost:5173",
 };
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
 //TEMP__TESTING
-app.post('/api/webhooks', express.raw({ type: 'application/json' }), webhookHandler);
+app.post(
+  "/api/webhooks",
+  express.raw({ type: "application/json" }),
+  webhookHandler
+);
 
 //app.use('/api/webhooks', express.raw({ type: 'application/json' }));
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors(corsOption));
-app.use("/", eventRoutes)
+app.use("/", eventRoutes);
 
 //app.post ('/api/webhooks', webhookHandler);
 
-app.get('/protected', requireAuth(), async (req, res) => {
-    const { userId } = getAuth(req)
-    res.json({userId})
-})
+app.get("/protected", requireAuth(), async (req, res) => {
+  const { userId } = getAuth(req);
+  res.json({ userId });
+});
 
-
-app.use('/api/profiles', profileRoutes);
+app.use("/api/profiles", profileRoutes);
 app.use("/api/neighborhoods", neighborhoodRoutes);
 
-app.get('/', requireAuth(), async (req, res) => {
-    const { userId } = getAuth(req);
-    const user = await clerkClient.users.getUser(userId);
-    return res.json({ user });
-})
+app.get("/", requireAuth(), async (req, res) => {
+  const { userId } = getAuth(req);
+  const user = await clerkClient.users.getUser(userId);
+  return res.json({ user });
+});
+
+app.listen(PORT, () => {
+    console.log(`Testing on ${PORT} `)
+});
