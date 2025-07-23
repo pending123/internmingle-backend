1d1c508242455f707e1aba83c09f7356dfb7a94d
Divine Precious-Esue <divinepreciousesue@gmail.com>
2025-07-23 16:32:51 -0700

    Deployment Prep

--- server.js
+++ server.js
@@ -1,51 +1,56 @@
 require("dotenv/config");
 const express = require("express");
+const cors = require("cors");
 const { clerkClient, requireAuth, getAuth } = require("@clerk/express");
+
 const neighborhoodRoutes = require("./src/routes/neighborhoodRoutes");
 const profileRoutes = require("./src/routes/profileRoutes");
+const eventRoutes = require("./src/routes/eventRoutes");
 const { webhookHandler } = require("./src/controllers/clerkWebhooks");
 
-const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const eventRoutes = require("./src/routes/eventRoutes");
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://internmingle.tech"
+];
 
-const corsOption = {
-  origin: "http://localhost:5173",
-  credentials: true //test
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
 };
 
-const app = express();
-const PORT = 3000;
+app.use(cors(corsOptions));
+app.use(express.json());
 
-//TEMP__TESTING
 app.post(
   "/api/webhooks",
   express.raw({ type: "application/json" }),
   webhookHandler
 );
 
-//app.use('/api/webhooks', express.raw({ type: 'application/json' }));
-
-app.use(express.json());
-app.use(cors(corsOption));
+app.use("/api/profiles", profileRoutes);
+app.use("/api/neighborhoods", neighborhoodRoutes);
 app.use("/", eventRoutes);
-
-//app.post ('/api/webhooks', webhookHandler);
 
 app.get("/protected", requireAuth(), async (req, res) => {
   const { userId } = getAuth(req);
   res.json({ userId });
 });
 
-app.use("/api/profiles", profileRoutes);
-app.use("/api/neighborhoods", neighborhoodRoutes);
-
 app.get("/", requireAuth(), async (req, res) => {
   const { userId } = getAuth(req);
   const user = await clerkClient.users.getUser(userId);
-  return res.json({ user });
+  res.json({ user });
 });
 
 app.listen(PORT, () => {
-    console.log(`Testing on ${PORT} `)
+  console.log(`Server is running on port ${PORT}`);
 });
